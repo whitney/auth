a528fec3df78a53fdc691e1badc6713da6376405
Whitney Zoller <whitneyrzoller@gmail.com>
2013-11-20 16:54:15 -0500

    signup, login, logout implemented

--- auth.go
+++ auth.go
@@ -5,6 +5,7 @@ import (
   "log"
   "net/http"
   "os"
+  "time"
   "strconv"
   "encoding/base64"
   "encoding/json"
@@ -12,6 +13,10 @@ import (
   _ "github.com/lib/pq"
   "github.com/gorilla/securecookie"
   "code.google.com/p/go.crypto/bcrypt"
+)
+
+const (
+  cookieName string = "AUTHID"
 )
 
 var db *sqlx.DB
@@ -28,7 +33,8 @@ func main() {
 
   http.HandleFunc("/auth/signup", createUser)
   http.HandleFunc("/auth/login", login)
-  http.HandleFunc("/auth/test", test)
+  http.HandleFunc("/auth/logout", logout)
+  http.HandleFunc("/auth/authenticated", authenticated)
 
   log.Println("listening...")
   err = http.ListenAndServe(":"+os.Getenv("AUTH_PORT"), nil)
@@ -44,11 +50,30 @@ type User struct {
   AuthToken      string `db:"auth_token"`
 }
 
-func test(res http.ResponseWriter, req *http.Request) {
+func authenticated(res http.ResponseWriter, req *http.Request) {
   res.Header().Set("Content-Type", "application/json") 
-  authTkn := base64.URLEncoding.EncodeToString(securecookie.GenerateRandomKey(32)) 
-  log.Printf("len(authTkn): %s", len(authTkn))
-  fmt.Fprintln(res, "authTkn: " + authTkn)
+  authTkn, err := readAuthCookie(req)
+  if err != nil {
+    http.Error(res, err.Error(), http.StatusUnauthorized)
+    return
+  }
+
+  user, err := queryUserByAuthTkn(authTkn)
+  if err != nil {
+    http.Error(res, err.Error(), http.StatusNotFound)
+    return
+  }
+
+  uMap := make(map[string]string)
+  uMap["id"] = strconv.Itoa(int(user.Id))
+  uMap["username"] = user.Username
+  jsonStr, err := jsonWrapMap(uMap)
+  if err != nil {
+    http.Error(res, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  fmt.Fprintln(res, jsonStr)
 }
 
 func createUser(res http.ResponseWriter, req *http.Request) {
@@ -66,7 +91,7 @@ func createUser(res http.ResponseWriter, req *http.Request) {
     return
   }
 
-  _, err := queryUser(username)
+  _, err := queryUserByUsername(username)
   if err == nil {
     http.Error(res, "username taken", http.StatusBadRequest)
     return
@@ -77,7 +102,6 @@ func createUser(res http.ResponseWriter, req *http.Request) {
     http.Error(res, err.Error(), http.StatusInternalServerError)
     return
   }
-  log.Printf("hashedPwd: %s", string(hashedPwd))
 
   authTkn := base64.URLEncoding.EncodeToString(securecookie.GenerateRandomKey(32)) 
   log.Printf("authTkn: %s", authTkn)
@@ -100,6 +124,74 @@ func createUser(res http.ResponseWriter, req *http.Request) {
   fmt.Fprintln(res, string(json))
 }
 
+func readAuthCookie(req *http.Request) (authTkn string, err error) {
+  cookie, err := req.Cookie(cookieName)
+  if err != nil {
+    return authTkn, err
+  }
+
+  hashKey := []byte("very-secret")
+  var blockKey []byte
+  if os.Getenv("COOKIE_SEC") == "encrypted" {
+    blockKey = []byte("a-lot-secret")
+  } else {
+    blockKey = nil
+  }
+  s := securecookie.New(hashKey, blockKey)
+  err = s.Decode(cookieName, cookie.Value, &authTkn)
+  return authTkn, err
+}
+
+func setAuthCookie(authTkn string, res http.ResponseWriter) (err error) {
+  hashKey := []byte("very-secret")
+  var blockKey []byte
+  var secureCookie bool
+  if os.Getenv("COOKIE_SEC") == "encrypted" {
+    blockKey = []byte("a-lot-secret")
+    secureCookie = true
+  } else {
+    blockKey = nil
+    secureCookie = false
+  }
+  s := securecookie.New(hashKey, blockKey)
+  encoded, err := s.Encode(cookieName, authTkn)
+  expiry := time.Now().Add(24*365*time.Hour)
+  if err == nil {
+    cookie := &http.Cookie{
+      Name:  cookieName,
+      Value: encoded,
+      Path:  "/",
+      HttpOnly: true,
+      Secure: secureCookie,
+      Expires: expiry,
+    }
+    http.SetCookie(res, cookie)
+  }
+  return err
+}
+
+func invalidateAuthCookie(res http.ResponseWriter) (err error) {
+  var secureCookie bool
+  if os.Getenv("COOKIE_SEC") == "encrypted" {
+    secureCookie = true
+  } else {
+    secureCookie = false
+  }
+  expiry := time.Now().Add(-24*time.Hour)
+  if err == nil {
+    cookie := &http.Cookie{
+      Name:  cookieName,
+      Value: "xxx",
+      Path:  "/",
+      HttpOnly: true,
+      Secure: secureCookie,
+      Expires: expiry,
+    }
+    http.SetCookie(res, cookie)
+  }
+  return err
+}
+
 func login(res http.ResponseWriter, req *http.Request) {
   res.Header().Set("Content-Type", "application/json") 
 
@@ -115,7 +207,7 @@ func login(res http.ResponseWriter, req *http.Request) {
     return
   }
 
-  user, err := queryUser(username)
+  user, err := queryUserByUsername(username)
   if err != nil {
     http.Error(res, err.Error(), http.StatusNotFound)
     return
@@ -137,7 +229,23 @@ func login(res http.ResponseWriter, req *http.Request) {
     return
   }
 
+  err = setAuthCookie(user.AuthToken, res)
+  if err != nil {
+    http.Error(res, err.Error(), http.StatusInternalServerError)
+    return
+  }
+
   fmt.Fprintln(res, jsonStr)
+}
+
+func logout(res http.ResponseWriter, req *http.Request) {
+  res.Header().Set("Content-Type", "application/json") 
+  err := invalidateAuthCookie(res)
+  if err != nil {
+    http.Error(res, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  fmt.Fprintln(res, "{'msg': 'ok'}")
 }
 
 func insertUser(username string, pwdDigest string, authTkn string) (uId int64, err error) {
@@ -149,12 +257,20 @@ func insertUser(username string, pwdDigest string, authTkn string) (uId int64, e
     return uId,err
   }
 
-  user, err := queryUser(username)
+  user, err := queryUserByUsername(username)
   return user.Id, err
 }
 
-func queryUser(username string) (u User, err error) {
+func queryUserByUsername(username string) (u User, err error) {
   err = db.Get(&u, "SELECT * FROM users WHERE username=$1", username)
+  if err != nil {
+    return u,err
+  }
+  return u,nil
+}
+
+func queryUserByAuthTkn(authTkn string) (u User, err error) {
+  err = db.Get(&u, "SELECT * FROM users WHERE auth_token=$1", authTkn)
   if err != nil {
     return u,err
   }
